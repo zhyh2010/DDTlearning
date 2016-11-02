a8ea1b0ca7f59e5c0117f1ae08dfdd6a7509adaf
zhyh2010 <1435589631@qq.com>
2016-11-02 21:58:20 +0800

    add zeroPad

--- Soundex/SoundexTest.cpp
+++ Soundex/SoundexTest.cpp
@@ -1,11 +1,30 @@
-class Soundex{
+#include <string>
 
+class Soundex{
+public:
+	std::string encode(const std::string & word) const{
+		return zeroPad(word);
+	}
+
+private:
+	std::string zeroPad(const std::string & word) const{
+		return word + "000";
+	}
 };
 
 #include "gtest/gtest.h"
 
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord){
 	Soundex soundex;
+
+	auto encoded = soundex.encode("A");
+	ASSERT_EQ(encoded, std::string("A000"));
+}
+
+TEST(SoundexEncoding, PadsWithZerosToEnsureThreeDigits){
+	Soundex soundex;
+	auto encoded = soundex.encode("I");
+	ASSERT_EQ(encoded, std::string("I000"));
 }
 
 int main(int argc, char ** argv){
